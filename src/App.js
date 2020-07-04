3ac1f8a161aa2907f9faa51df6da065c8574a2ba
Spring Nguyen <springnguyen98@gmail.com>
2020-07-03 23:34:37 -0700

    Get started on main screen, still needs work for mobile support menu

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ class App extends React.Component {
 
     this.state = {
       height: 0,
-      width: 0
+      width: 0,
+      menuExpand: false,
+      page: "Home"
     };
 
     window.addEventListener("resize", this.update);
@@ -21,29 +23,64 @@ class App extends React.Component {
   update = () => {this.setState({height: window.innerHeight, width: window.innerWidth});};
 
   render() {
-    let navigation = <button>&#8801;</button>;
-    if (this.state.width > 500){
+    let navigation = <button onClick={() => this.setState({menuExpand: !this.state.menuExpand})}>&#8801;</button>;
+
+    // if screen width is large, use horizontal navigation instead of button
+    if (this.state.width > 700){
       navigation =
       <nav>
         <ul>
-          <li>About</li>
-          <li>Portfolio</li>
-          <li>Blog</li>
-          <li>Contact</li>
+          <li><button onClick={() => this.setState({page: "About"})}>About</button></li>
+          <li><button onClick={() => this.setState({page: "Portfolio"})}>Portfolio</button></li>
+          <li><button onClick={() => this.setState({page: "Blog"})}>Blog</button></li>
+          <li><button onClick={() => this.setState({page: "Contact"})}>Contact</button></li>
         </ul>
       </nav>;
     }
 
+    let header =
+    <header>
+      <div className="header_withButton">
+        <div></div>
+        <h1>Spring Nguyen</h1>
+        {navigation}
+      </div>
+    </header>;
+
+    // if screen width is large, use different css
+    if (this.state.width > 700){
+      header =
+      <header className="header_noButton">
+        <h1>Spring Nguyen</h1>
+        {navigation}
+      </header>;
+    }
+
+    if (this.state.width < 700 && this.state.menuExpand){
+      header =
+      <header>
+        <div className="header_withButton">
+          <div></div>
+          <h1>Spring Nguyen</h1>
+          {navigation}
+        </div>
+        <ul className="ul_withButtonPressed">
+          <li><button onClick={() => this.setState({page: "About"})}>About</button></li>
+          <li><button onClick={() => this.setState({page: "Portfolio"})}>Portfolio</button></li>
+          <li><button onClick={() => this.setState({page: "Blog"})}>Blog</button></li>
+          <li><button onClick={() => this.setState({page: "Contact"})}>Contact</button></li>
+        </ul>;
+      </header>;
+    }
+
     return (
       <div>
-        <div class="fullscreen-bg">
-          <video autoPlay muted loop class="fullscreen-bg_video">
+        <div className="fullscreen-bg">
+          <video autoPlay muted loop className="fullscreen-bg_video">
             <source src={backgroundVideo} type="video/mp4"></source>
           </video>
         </div>
-        <header>
-          {navigation}
-        </header>
+        {header}
       </div>
     );
   }
