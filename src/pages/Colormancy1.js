edd60a0eb27675dc8ba437d7e0dc13269e93e1e0
mandarker <springnguyen98@gmail.com>
2022-03-20 07:10:46 -0700

    IAIDO page

--- src/pages/Colormancy1.js
+++ src/pages/Colormancy1.js
@@ -14,7 +14,7 @@ class Colormancy extends React.Component {
         <h1>Colormancy VFX</h1>
         <video loop autoPlay muted src={colormancy1Preview}></video>
         <h2>Making spell effects for an action packed painting game.</h2>
-        <p>These are my VFX contributions to Colormancy, a game where players must cast spells to paint the environment.
+        <p>These are some of my VFX contributions to Colormancy, a game where players must cast spells to paint the environment.
         Fully painting the environment and dispatching enemies will win the game.</p>
         <p>Each effect has their own written shaders in HLSL and geometry.</p>
         <iframe src="https://player.vimeo.com/video/533730360?autoplay=1&loop=1&autopause=0" frameborder="0" allow="autoplay"></iframe>
